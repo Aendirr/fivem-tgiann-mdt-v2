9aba74d2f90f36757efa051522dbe85374ef2792
aendirr <118621878+Aendirr@users.noreply.github.com>
2023-08-03 09:07:03 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,86 +1,128 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+QBCore = exports['qb-core']:GetCoreObject()
 
 local players = {}
 
 QBCore.Functions.CreateCallback("tgiann-mdtv2:ilk-data", function(source, cb)
-    local xPlayer = QBCore.Functions.GetPlayer(source)
-    exports.oxmysql:execute("SELECT firstname, lastname, job FROM users WHERE identifier = @identifier", {
-        ["@identifier"] = xPlayer.identifier
-    }, function (result)
-        if result[1] then
-            cb(players, QBCore.Shared.Items, result[1].firstname.." "..result[1].lastname)
-        end
-    end)
+    cb(players)
 end)
 
 Citizen.CreateThread(function()
-    exports.oxmysql:execute("SELECT firstname, lastname, job FROM users ", {
+    exports.oxmysql:execute("SELECT charinfo, job FROM players ", {
     }, function (result)
         players.police = {}
         players.user = {}
         for i=1, #result do
-            if result[i].job == "police" then
-                table.insert(players.police, result[i].firstname .. " " .. result[i].lastname)
+            local charinfo = json.decode(result[i].charinfo)
+            if result[i].job.name == "police" then
+                table.insert(players.police, charinfo.firstname .. " " .. charinfo.lastname)
             else
-                table.insert(players.user, result[i].firstname .. " " .. result[i].lastname)
+                table.insert(players.user, charinfo.firstname .. " " .. charinfo.lastname)
             end
         end
-    end) 
+    end)
 end)
 
-RegisterCommand("mdt", function(source, args)
+QBCore.Commands.Add("tablet", "EMS/Polis Tabletini Aç", {}, false, function(source, args) -- name, help, arguments, argsrequired,  end sonuna persmission
 	TriggerClientEvent('tgiann-mdtv2:open', source)
+    TriggerClientEvent("tgiann-emstab:open", source)
+end)
+
+QBCore.Functions.CreateUseableItem("pmdt", function(source, item)
+    TriggerClientEvent("tgiann-mdtv2:open", source)
+end)
+
+QBCore.Functions.CreateUseableItem("emdt", function(source, item)
+    TriggerClientEvent("tgiann-emstab:open", source)
+end)
+
+QBCore.Commands.Add("tabletzoom", 'Tabletin Boyutunu Ayarlar.', {{ name="Tablet Boyutu", help="50 İle 100 Arası Bir Değer"}}, false, function(source, args) -- name, help, arguments, argsrequired,  end sonuna persmission
+	TriggerClientEvent('tgiann-mdtv2:zoom', source, args[1])
+    TriggerClientEvent("tgiann-emstab:zoom", source, args[1])
 end)
 
 QBCore.Functions.CreateCallback("tgiann-mdtv2:sorgula", function(source, cb, data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+
     if data.tip == "isim" then
-        exports.oxmysql:execute("SELECT * FROM users WHERE CONCAT(firstname, ' ', lastname) LIKE @search LIMIT 30", {
-            ['@search'] = '%'..data.data..'%'
-        }, function (result)
-            if result then
-                cb(result)
+        local foundPlayers = {}
+        exports.oxmysql:execute("SELECT * FROM players", {}, function (result)
+            if result[1] then
+                for k, v in pairs(result) do
+                    v.charinfo = json.decode(v.charinfo)
+                    v.ehliyetceza = json.decode(v.metadata).ehliyetceza
+                    v.money = json.decode(v.money)
+                    v.aranma = json.decode(v.aranma)
+                    if string.find(v.charinfo.firstname, data.data) or string.find(v.charinfo.lastname, data.data) then
+                        table.insert(foundPlayers, v)
+                    end
+                end
+
+                cb(foundPlayers)
             end
-        end) 
+        end)
     elseif data.tip == "arac" then
-        exports.oxmysql:execute("SELECT * FROM owned_vehicles WHERE owner = @owner", {
-            ['@owner'] = data.data
+        exports.oxmysql:execute("SELECT * FROM player_vehicles WHERE citizenid = @citizenid", {
+            ['@citizenid'] = data.data
         }, function (result)
             if result then
                 cb(result)
             end
         end) 
     elseif data.tip == "numara" then
-        exports.oxmysql:execute("SELECT * FROM users WHERE phone_number LIKE @search LIMIT 30", {
+        exports.oxmysql:execute("SELECT * FROM players WHERE phone LIKE @search LIMIT 30", {
             ['@search'] = '%'..data.data..'%'
         }, function (result)
-            if result then
-                cb(result)
+            local foundPlayers = {}
+            if result[1] then
+                for k, v in pairs(result) do
+                    v.charinfo = json.decode(v.charinfo)
+                    v.ehliyetceza = json.decode(v.metadata).ehliyetceza
+                    v.money = json.decode(v.money)
+                    v.aranma = json.decode(v.aranma)
+                    table.insert(foundPlayers, v)
+                end
+                cb(foundPlayers)
             end
         end) 
     elseif data.tip == "plaka" then
-        exports.oxmysql:execute("SELECT * FROM owned_vehicles LEFT JOIN users ON owned_vehicles.owner = users.identifier WHERE owned_vehicles.plate LIKE @plate LIMIT 30", {
+        exports.oxmysql:execute("SELECT * FROM player_vehicles LEFT JOIN players ON player_vehicles.citizenid = players.citizenid WHERE player_vehicles.plate LIKE @plate LIMIT 30", {
             ['@plate'] = '%'..data.data..'%'
         }, function (result)
+            local foundPlayers = {}
             if result then
-                cb(result)
+                for k, v in pairs(result) do
+                    v.charinfo = json.decode(v.charinfo)
+                    v.ehliyetceza = json.decode(v.metadata).ehliyetceza
+                    v.money = json.decode(v.money)
+                    v.aranma = json.decode(v.aranma)
+                    table.insert(foundPlayers, v)
+                end
+
+                cb(foundPlayers)
             end
         end) 
     end
 end)
 
-QBCore.Functions.CreateCallback("tgiann-mdtv2:photo", function(source, cb, data)
-    exports.oxmysql:execute("SELECT photo FROM users WHERE identifier = @identifier", {
-        ['@identifier'] = data.data
-    }, function (result)
-        if result then
-            cb(result[1].photo)
-        end
-    end) 
+QBCore.Functions.CreateCallback('tgiann-mdtv2:ev', function(source, cb, citizenid)
+    -- exports.oxmysql:execute("SELECT * FROM tgiann_ev WHERE (id = @id OR anahtar1 = @id OR anahtar2 = @id OR anahtar3 = @id OR anahtar4 = @id OR anahtar5 = @id)", {
+    --     ['@id'] = citizenid
+    -- }, function(result)
+    --     if result[1] then
+    --         cb(result[1].id)
+    --     else
+    --         cb("Ev Bilgisi Yok!")
+    --     end
+    -- end)
+
+    cb("Ev Bilgisi Yok!")
 end)
 
 RegisterServerEvent('tgiann-mdtv2:ceza-kaydet')
 AddEventHandler('tgiann-mdtv2:ceza-kaydet', function(data)
     local src = source
+
     exports.oxmysql:execute("INSERT INTO tgiann_mdt_olaylar SET aciklama = @aciklama, polis = @polis, zanli = @zanli, esyalar = @esyalar", {
         ["@aciklama"] = data.aciklama,
         ["@polis"] = json.encode(data.polis),
@@ -88,39 +130,42 @@ AddEventHandler('tgiann-mdtv2:ceza-kaydet', function(data)
         ["@esyalar"] = json.encode(data.esyalar),
      }, function(result1)
         for i=1, #data.zanli do
-            exports.oxmysql:execute("SELECT identifier FROM users WHERE CONCAT(firstname, ' ', lastname) LIKE @search LIMIT 30", {
-                ['@search'] = '%'..data.zanli[i]..'%'
-            }, function(result)
+            exports.oxmysql:execute("SELECT * FROM players", {}, function(result)
                 if result[1] then
-                   --[[  if data.illegal then
-                        local tPlayer = QBCore.Functions.GetPlayerByCitizenId(result[1].citizenid)
-                        if tPlayer and not tPlayer.PlayerData.metadata.illegalv2 then
-                            tPlayer.Functions.SetMetaData("illegalv2", true)
-                            TriggerClientEvent("tgiann-base:set-illegal", tPlayer.PlayerData.source, QBCore.Key)
-                        end
-                    end ]]
-
-                    --[[ if data.ceza.tceza > 0 then
-                        local tPlayer = QBCore.Functions.GetPlayerByCitizenId(result[1].citizenid)
-                        if tPlayer and tPlayer.PlayerData.metadata.ehliyetceza < 10 then
-                            local newValue = tPlayer.PlayerData.metadata.ehliyetceza+data.ceza.tceza
-                            tPlayer.Functions.SetMetaData("ehliyetceza", newValue)
-                            if newValue > 9 then
-                                TriggerClientEvent("QBCore:Notify", tPlayer.PlayerData.source, "Ehliyetine Polis Tarafından El Konuldu", "error", 15000)
-                                TriggerClientEvent("QBCore:Notify", src, tPlayer.PlayerData.charinfo.firstname.. " " ..tPlayer.PlayerData.charinfo.lastname.." İsimli Kişinin Ehliyetine El Konuldu", "success", 15000)
+                    for k, v in pairs(result) do
+                        v.charinfo = json.decode(v.charinfo)
+                        if v.charinfo.firstname ..' '.. v.charinfo.lastname == data.zanli[i] then
+                            if data.illegal then
+                                local tPlayer = QBCore.Functions.GetPlayerByCitizenId(v.citizenid)
+                                if tPlayer and not tPlayer.PlayerData.metadata.illegal then
+                                    tPlayer.Functions.SetMetaData("illegal", true)
+                                    TriggerClientEvent("tgiann-base:set-illegal", tPlayer.PlayerData.source, QBCore.Key)
+                                end
+                            end 
+        
+                            if data.ceza.tceza > 0 then
+                                local tPlayer = QBCore.Functions.GetPlayerByCitizenId(v.citizenid)
+                                if tPlayer and tPlayer.PlayerData.metadata.ehliyetceza < 0 then
+                                    local newValue = tPlayer.PlayerData.metadata.ehliyetceza+data.ceza.tceza
+                                    tPlayer.Functions.SetMetaData("ehliyetceza", newValue)
+                                    if newValue > 9 then
+                                        TriggerClientEvent("QBCore:Notify", tPlayer.PlayerData.source, "Ehliyetine Polis Tarafından El Konuldu", "error", 15000)
+                                        TriggerClientEvent("QBCore:Notify", src, tPlayer.PlayerData.charinfo.firstname.. " " ..tPlayer.PlayerData.charinfo.lastname.." İsimli Kişinin Ehliyetine El Konuldu", "success", 15000)
+                                    end
+                                end
                             end
+                            
+                            exports.oxmysql:execute("INSERT INTO tgiann_mdt_cezalar SET citizenid = @citizenid, aciklama = @aciklama, ceza = @ceza, polis = @polis, cezalar = @cezalar, zanli = @zanli, olayid = @id", {
+                                ["@citizenid"] = v.citizenid,
+                                ["@aciklama"] = data.aciklama,
+                                ["@ceza"] = json.encode(data.ceza),
+                                ["@polis"] = json.encode(data.polis),
+                                ["@zanli"] = json.encode(data.zanli),
+                                ["@cezalar"] = data.cezaisim,
+                                ["@id"] = result1.insertId
+                            })     
                         end
-                    end ]]
-                    
-                    exports.oxmysql:execute("INSERT INTO tgiann_mdt_cezalar SET citizenid = @citizenid, aciklama = @aciklama, ceza = @ceza, polis = @polis, cezalar = @cezalar, zanli = @zanli, olayid = @id", {
-                        ["@citizenid"] = result[1].identifier,
-                        ["@aciklama"] = data.aciklama,
-                        ["@ceza"] = json.encode(data.ceza),
-                        ["@polis"] = json.encode(data.polis),
-                        ["@zanli"] = json.encode(data.zanli),
-                        ["@cezalar"] = data.cezaisim,
-                        ["@id"] = result1.insertId
-                    })
+                    end
                 end
             end)
         end
@@ -128,17 +173,42 @@ AddEventHandler('tgiann-mdtv2:ceza-kaydet', function(data)
 end)
 
 QBCore.Functions.CreateCallback("tgiann-mdtv2:olaylardata", function(source, cb, data)
+    local data = {}
     exports.oxmysql:execute("SELECT * FROM tgiann_mdt_olaylar ORDER BY id DESC LIMIT 100", {
     }, function (result)
-        cb(result)
+        if result[1] then
+            for k, v in pairs(result) do
+                v.ceza = json.decode(v.ceza)
+                v.polis = json.decode(v.polis)
+                v.zanli = json.decode(v.zanli)
+                v.esyalar = json.decode(v.esyalar)
+
+                table.insert(data, v)
+            end
+
+            cb((data))
+        end
+        
     end) 
 end)
 
 QBCore.Functions.CreateCallback("tgiann-mdtv2:sabikadata", function(source, cb, data)
+    local found = {}
     exports.oxmysql:execute("SELECT * FROM tgiann_mdt_cezalar WHERE citizenid = @citizenid ORDER BY id DESC ", {
         ["@citizenid"] = data
     }, function (result)
-        cb(result)
+        if result[1] then
+            for k, v in pairs(result) do
+                v.ceza = json.decode(v.ceza)
+                v.polis = json.decode(v.polis)
+                v.zanli = json.decode(v.zanli)
+                v.esyalar = json.decode(v.esyalar)
+
+                table.insert(found, v)
+            end
+
+            cb((found))
+        end
     end) 
 end)
 
@@ -151,10 +221,11 @@ end)
 
 RegisterServerEvent('tgiann-mdtv2:setavatar')
 AddEventHandler('tgiann-mdtv2:setavatar', function(url, id)
-    exports.oxmysql:execute("UPDATE users SET photo=@photo WHERE identifier = @identifier", {
-        ['@identifier'] = id,
-        ['@photo'] = url
-    })
+    local xPlayer = QBCore.Functions.GetPlayerByCitizenId(id)
+    if xPlayer then
+        xPlayer.Functions.SetCharInfo("photo", url)
+        xPlayer.Functions.Save()
+    end
 end)
 
 RegisterServerEvent('tgiann-mdtv2:olaysil')
@@ -169,13 +240,18 @@ end)
 
 RegisterServerEvent('tgiann-mdtv2:aranma')
 AddEventHandler('tgiann-mdtv2:aranma', function(data, durum)
+    local xPlayer = QBCore.Functions.GetPlayerByCitizenId(data.id)
     if durum then
         local saat = os.time() + data.saat * 86400
-        exports.oxmysql:execute("UPDATE users SET aranma=@aranma WHERE identifier = @identifier", {
-            ['@identifier'] = data.id,
-            ['@aranma'] = json.encode({durum = true, sebep=data.neden, suansaat=os.time(), saat=saat})
-        })
-
+        if xPlayer then
+            xPlayer.Functions.SetAranma(true, data.neden, saat)
+            xPlayer.Functions.Save()
+        else
+            exports.oxmysql:execute("UPDATE players SET aranma=@aranma WHERE citizenid = @citizenid", {
+                ['@citizenid'] = data.id,
+                ['@aranma'] = json.encode({durum = true, sebep=data.neden, suansaat=os.time(), saat=saat})
+            })
+        end
         exports.oxmysql:execute("INSERT INTO tgiann_mdt_arananlar SET citizenid = @citizenid, sebep = @sebep, baslangic = @baslangic, bitis = @bitis, isim = @isim", {
             ["@citizenid"] = data.id,
             ["@sebep"] = data.neden,
@@ -184,11 +260,15 @@ AddEventHandler('tgiann-mdtv2:aranma', function(data, durum)
             ["@isim"] = data.isim
         })
     else
-        exports.oxmysql:execute("UPDATE users SET aranma=@aranma WHERE identifier = @identifier", {
-            ['@identifier'] = data.id,
-            ['@aranma'] = json.encode({durum = false, sebep="", suansaat="", saat=""})
-        })
-        
+        if xPlayer then
+            xPlayer.Functions.SetAranma(false)
+            xPlayer.Functions.Save()
+        else
+            exports.oxmysql:execute("UPDATE players SET aranma=@aranma WHERE citizenid = @citizenid", {
+                ['@citizenid'] = data.id,
+                ['@aranma'] = json.encode({durum = false, sebep="", suansaat="", saat=""})
+            })
+        end
         exports.oxmysql:execute("DELETE FROM tgiann_mdt_arananlar WHERE citizenid = @citizenid", {
             ['@citizenid'] = data.id
         })
@@ -203,9 +283,21 @@ QBCore.Functions.CreateCallback("tgiann-mdtv2:arananlar", function(source, cb, d
 end)
 
 QBCore.Functions.CreateCallback("tgiann-mdtv2:olayara", function(source, cb, data)
+    local found = {}
     exports.oxmysql:execute("SELECT * FROM tgiann_mdt_olaylar WHERE id = @id", {
         ["@id"] = tonumber(data)
     }, function (result)
-        cb(result)
+        if result[1] then
+            for k, v in pairs(result) do
+                v.ceza = json.decode(v.ceza)
+                v.polis = json.decode(v.polis)
+                v.zanli = json.decode(v.zanli)
+                v.esyalar = json.decode(v.esyalar)
+
+                table.insert(found, v)
+            end
+
+            cb((found))
+        end
     end) 
 end)
